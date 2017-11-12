7cf2a7cbe2bdfa3102ab99bced0980edad788535
kac487 <kac487>
2017-11-12 17:23:52 -0500

    Added word generator

---
+++ wordGen.py
@@ -0,0 +1,14 @@
+import requests
+import time
+import random
+
+word_site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+
+response = requests.get(word_site)
+WORDS = response.content.splitlines()
+
+random.shuffle(WORDS)
+
+for i in WORDS:
+ q    print(str(i))
+    time.sleep(1.5)
